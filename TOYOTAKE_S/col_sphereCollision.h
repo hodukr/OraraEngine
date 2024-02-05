98598ae800b26b9b52c83955e1ec0a15993b136e
Toyo1007 <toyotake1007@gmail.com>
2024-02-05 15:33:34 +0900

    自分が作ったソースファイル追加

---
+++ TOYOTAKE_S/col_sphereCollision.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include"com_collisionShape.h"
+
+class SphereCollision : public CollisionShape
+{
+private:
+    #define VERTEX_NUM  31
+
+    ID3D11Buffer* m_VertexBuffer[3]{};
+
+    float   m_Size;
+
+    void SetVertex(VERTEX_3D* vertex, int index);
+public:
+    SphereCollision(){ m_Shape = SHAPE_SPHERE; }
+
+    void Init() override;
+    void Uninit() override;
+    void Update() override;
+    void Draw() override;
+
+    void SetSize(float size) { m_Size = size; }
+    float GetSize() { return m_Size; }
+
+    bool CheckCollision(CollisionShape* other) override { return other->CollideWith(this); }
+    bool CollideWith(BoxCollision* other) override;
+    bool CollideWith(SphereCollision* other) override;
+};
---
+++ TOYOTAKE_S/guiw_accessFolder.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "imgui/imgui.h"
+#include "imGuiWindow.h"
+#include <set>
+#include <list>
+#include <unordered_map>
+#include <filesystem>  // C++17の標準ライブラリに含まれているヘッダ
+namespace fs = std::filesystem;
+#define MAX_TEXT_NUM 256
+
+class AccessFolder:public ImGuiWindow
+{
+private:
+	bool m_CreateFolder = false;
+	bool m_IsWindw = false;
+	char m_CreatedFolderName[MAX_TEXT_NUM]{};
+	char m_FolderName[MAX_TEXT_NUM]{};                     //最大文字数256
+	std::set<std::string> m_ProjectFolders{};              //assetフォルダの中身のフォルダ名
+	std::string m_ProjectFolderName{};                     //今開いているフォルダの名前を格納
+	std::unordered_map<std::string, std::string> m_Path{}; //名前をKeyにパスを格納
+	float m_ImageSize = 30.0f;
+	std::string m_DragName{};   //ドラッグされたものの名前
+
+
+	void CreateFolder();
+	void DrawFolderIconAndName(const char* name, ImVec2 size, ImVec2 uv);
+public:
+	void Draw()override;
+	std::string GetNowFolder() { return m_ProjectFolderName; }
+	std::string GetDragName() { return m_DragName; }
+};
